2e2940db2e860e372672b98c6bbcdca5488dfacc
wilfriedpapt89 <wilfriedpapt@gmail.com>
2022-03-23 18:57:09 +0200

    setup project

---
+++ src/main/java/com/akenge/vaadincrudproject/VaadinCrudProjectApplication.java
@@ -0,0 +1,13 @@
+package com.akenge.vaadincrudproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VaadinCrudProjectApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(VaadinCrudProjectApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/akenge/vaadincrudproject/VaadinCrudProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.akenge.vaadincrudproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VaadinCrudProjectApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
